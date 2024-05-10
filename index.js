99232f35f95ca3af5f5aadb6221d5f6d56c2e3f9
dumindad <dumindad@gmail.com>
2024-05-10 22:14:38 +0530

    cors allow all origin and headers

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ const router = require('./routes')
 
 
 const app = express()
-app.use(cors({
-    origin : process.env.FRONTEND_URL,
-    credentials : true
-}))
+// app.use(cors({
+//     origin : process.env.FRONTEND_URL,
+//     credentials : true
+// }))
+app.use((_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+  
+    next();
+  });
+
 app.use(express.json())
 app.use(cookieParser())
 
