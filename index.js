42232eba5d2da8d12b5e61aeb12a93b0ec18afd1
dumindad <dumindad@gmail.com>
2024-05-10 22:27:42 +0530

    update access origin port 3000

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express()
 //     credentials : true
 // }))
 app.use((_req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://65.2.37.169:3000');
     res.header('Access-Control-Allow-Headers', '*');
   
     next();
